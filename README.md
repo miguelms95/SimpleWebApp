39bfc2be87a9d69e3d475b6d7e463d2b4c7cc97b
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2020-02-18 17:59:31 +0100

    Devise views to customize the registration fields.Bootstrap table display

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # SimpleWebApp
 
 Phone_number is a string in DB, but in the form it uses a number_field.
+
 The user's profile image is a string: image url.
+
 The user must fill the form, it's managed at controller's level. The Home Controller, and the User's Controller will redirect to the user edit form, if there is any required field.
 
-Things you may want to cover:
+Some info:
 
 * Ruby version: 2.5.0
 * Database creation: with mysql, db name: simple_app_db_development. It uses root user and  no password at database.yml.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   validates_presence_of :email
+  validates_presence_of :username
+  validates_presence_of :forename
+  validates_presence_of :surname
 
   def fields_completed
-    username.present? && forename.present? && surname.present? && email.present?
+    username.present? && forename.present? && surname.present? && email.present? && profile_image.present?
   end
 end
